8961c2e752aec856144e5d563795065534965496
bumfo <bumfo@users.noreply.github.com>
2019-12-30 18:09:48 +0800

    update test

--- glg2d/src/test/java/org/jogamp/glg2d/ben/CanvasTextTest.java
+++ glg2d/src/test/java/org/jogamp/glg2d/ben/CanvasTextTest.java
@@ -7,11 +7,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 
 public final class CanvasTextTest extends JFrame {
@@ -25,11 +25,16 @@ public final class CanvasTextTest extends JFrame {
         // g.drawRect(100, 100, 50, 50);
         Graphics2D g2 = (Graphics2D) g;
 
+        double s = 1. + 1. * (1. + Math.sin(t / 60));
+
+        g2.setTransform(AffineTransform.getScaleInstance(s, s));
+
         // g2.setFont(new Font("Arial", Font.PLAIN, 32));
 
-        g2.draw(new Rectangle2D.Double(100. + 50. * Math.sin(Math.PI * t / 60.), 100., 50., 50.));
+        double dx = 0.; // Math.sin(Math.PI * t / 60.);
+        g2.draw(new Rectangle2D.Double(100. + 50. * dx, 100., 50., 50.));
 
-        g2.drawString("Hello World", 100f + 50f * (float) Math.sin(Math.PI * t / 60.), 100f);
+        g2.drawString("Hello World", 100f + 50f * (float) dx, 100f);
 
         t += 1.;
       }
@@ -52,6 +57,12 @@ public final class CanvasTextTest extends JFrame {
       @Override
       public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+          if (animator.isPaused()) {
+            repaint();
+          } else {
+            animator.pause();
+          }
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
           if (animator.isPaused()) {
             animator.resume();
           } else {
